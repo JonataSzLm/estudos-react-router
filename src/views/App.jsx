922f39f8d466f3245a4b79ce452a93319026748e
JonataSzLm <jonaatslima5@gmail.com>
2022-09-17 15:35:04 -0300

    Modulo de Navegação com router

---
+++ src/views/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import './App.css'
+import { BrowserRouter } from "react-router-dom"
+
+import Menu from '../components/layout/Menu'
+import Content from '../components/layout/Content'
+
+const App = props => (
+    <div className="App">
+        <BrowserRouter>
+            <Menu></Menu>
+            <Content></Content>
+        </BrowserRouter>
+    </div>
+)
+
+export default App
